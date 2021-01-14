47062d02e2408fd225f039960194bf3fe2adb782
Gabriel Vieira <gabriel.vieira@envolvelabs.com>
2021-01-13 21:21:32 -0300

    New edit and save form

--- routes.js
+++ routes.js
@@ -20,6 +20,8 @@ routes.get('/instructors/:id', instructors.show)
 
 routes.post('/instructors', instructors.post)
 
+routes.put('/instructors', instructors.put)
+
 routes.get('/members', function(req, res ) {
     return res.send("members")
 })
